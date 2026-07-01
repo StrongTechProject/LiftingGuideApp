9b0543642215e219054b7b091c48f45e2933ec72
StrongTech <huilangid@gmail.com>
2026-07-01 23:28:13 +0800

    Initial Commit

---
+++ Lifting Guide/Lifting_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lifting_GuideApp.swift
+//  Lifting Guide
+//
+//  Created by Jacky on 2026/7/1.
+//
+
+import SwiftUI
+
+@main
+struct Lifting_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
